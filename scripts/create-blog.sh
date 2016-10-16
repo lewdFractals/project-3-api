8d1df440a178955817488a12f1f4fe6edd51d1d7
bullhead242 <bullhead242@gmail.com>
2016-10-15 22:09:05 -0400

    Change 'new-blog.sh' to 'create-blog' for increased semantic correctness Add 'destroy-blog' and 'get-blog' scripts to further test blog actions

---
+++ scripts/create-blog.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+curl --include --request POST http://localhost:3000/blogs \
+  --header "Authorization: Token token=/YkBQ1GEWDcDqFHfT8DN/ltguj8Ry+0W8jomFkKuj54=--Fd1tFRIlCM+M4a8VAYVn1RYQxGCfg3o0PrGh/4VudAc=" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "blog": {
+      "content": "wordswordwords",
+      "title": "shit",
+      "_owner": "req.currentUser._id"
+    }
+  }'
+
+  curl --include --request POST http://localhost:3000/examples \
+    --header "Authorization: Token token=/YkBQ1GEWDcDqFHfT8DN/ltguj8Ry+0W8jomFkKuj54=--Fd1tFRIlCM+M4a8VAYVn1RYQxGCfg3o0PrGh/4VudAc=" \
+    --header "Content-Type: application/json" \
+    --data '{
+      "example": {
+        "text": "cdflkjadfhjkadsfljkadsfhjadsfhladfslhjadslh",
+        "_owner": "req.currentUser._id"
+      }
+    }'
---
+++ scripts/get-blog.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl --include --request GET http://localhost:3000/blogs/5802dba3b675ee63ef6d6a27 \
+  --header "Authorization: Token token=/YkBQ1GEWDcDqFHfT8DN/ltguj8Ry+0W8jomFkKuj54=--Fd1tFRIlCM+M4a8VAYVn1RYQxGCfg3o0PrGh/4VudAc="
